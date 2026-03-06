026c03b6a79c90a4825fb1f4b9eabededc78395f
Mike88870687-cmyk <m88870687@gmail.com>
2026-03-06 22:38:16 +0200

    Initialize Spring Boot project Closes #1

---
+++ src/main/java/com/mike/redirect/MarketingRedirectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mike.redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingRedirectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingRedirectServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/redirect/MarketingRedirectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingRedirectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
